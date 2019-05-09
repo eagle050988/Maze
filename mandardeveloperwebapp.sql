8cff180b0b28d500d0d2cc68b07037a8b001a6e2
Wawan Firgiawan <wawanfirgiawan6@gmail.com>
2019-05-09 23:52:53 +0800

    Update and rename create-table.sql to mandardeveloperwebapp.sql

--- mandardeveloperwebapp.sql
+++ mandardeveloperwebapp.sql
@@ -1,7 +1,5 @@
 create table [dbo].[Registration](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    job VARCHAR(30),
-    date DATE
+    ID VARCHAR(30),
+    Name VARCHAR(30),
 );
