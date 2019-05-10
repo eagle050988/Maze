f9d652b5635bb42b6c15cc150c650d06925b116c
Wawan Firgiawan <wawanfirgiawan6@gmail.com>
2019-05-10 16:20:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,13 +17,12 @@
  </head>
  <body>
  <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+ <p>Isi nama dan alamat email Anda, lalu klik Kirim untuk mendaftar. <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
+       Name  <input type="text" name="name" id="Nama"/></br></br>
+       Email <input type="text" name="email" id="Email"/></br></br>
+       Job <input type="text" name="hobi" id="Hobi"/></br></br>
        <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
  </form>
  </body>
  </html>
